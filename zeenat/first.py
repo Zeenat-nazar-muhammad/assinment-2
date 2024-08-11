47d92f1fbb7f3d4c08dfa6933ae5f875fdfa3b95
Zeenat-nazar-muhammad <angbird74@gmail.com>
2024-08-11 12:05:18 +0500

    Add files via upload

---
+++ zeenat/first.py
@@ -0,0 +1,14 @@
+
+height=78
+base=60
+f=0.5
+area=f*base*height
+print("area of triangle",area)
+radius=8
+pie=3-14
+area=radius*pie
+print("area of circle",area)
+side=8
+side=9
+area=side*side
+print("area of square",area)
